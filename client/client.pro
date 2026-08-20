7466ccc60fc9eb2953d01f47d0f1505b3cb958d2
zxz <zxzzsdb@163.com>
2026-08-20 17:19:35 +0800

    day1 实现了登录界面和注册界面

---
+++ client/client.pro
@@ -0,0 +1,40 @@
+QT += widgets
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    logindialog.cpp \
+    main.cpp \
+    mainwindow.cpp \
+    registerdialog.cpp
+
+HEADERS += \
+    logindialog.h \
+    mainwindow.h \
+    registerdialog.h
+
+FORMS += \
+    logindialog.ui \
+    mainwindow.ui \
+    registerdialog.ui
+
+TRANSLATIONS += \
+    client_zh_CN.ts
+CONFIG += lrelease
+CONFIG += embed_translations
+
+RC_ICONS = icon.ico
+
+DESTDIR = ./bin
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    rc.qrc
---
+++ client/main.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+#include <QFile>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "client_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    QFile qss(":/style/stylesheet.qss");
+    if(qss.open(QFile::ReadOnly)){
+        qDebug("open success");
+        QString style = QLatin1String(qss.readAll());
+        a.setStyleSheet(style);
+        qss.close();
+    }else{
+        qDebug("open failed");
+    }
+    MainWindow w;
+    w.show();
+    return QCoreApplication::exec();
+}
